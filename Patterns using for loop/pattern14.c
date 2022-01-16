9d1890cda9f0b4e01f31f9d2243d43cf4ad5ea07
Soumyajit Roy <78533174+roysammy123@users.noreply.github.com>
2022-01-16 19:27:42 +0530

    Create pattern14.c

---
+++ Patterns using for loop/pattern14.c
@@ -0,0 +1,28 @@
+//Program to display a pattern
+
+#include<stdio.h>
+
+int main()
+{
+    int i,j,n;
+    printf("Enter the number of rows\n");
+    scanf("%d",&n);
+    printf("The desired output is:\n\n");
+
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n-i;j++)
+        {
+            printf(" ");
+        }
+
+        for(j=1;j<=(i*2)-1;j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
