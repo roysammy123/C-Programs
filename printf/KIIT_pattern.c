3c71b37cb552cfa41a04f95e79674c16172c733d
Soumyajit Roy <78533174+roysammy123@users.noreply.github.com>
2022-01-16 18:14:14 +0530

    Create KIIT_pattern.c

---
+++ printf/KIIT_pattern.c
@@ -0,0 +1,14 @@
+//Program to print a pattern of KIIT using only printf
+
+#include<stdio.h>
+
+int main()
+{
+    printf("            ___\n"); //12 spaces then 3 underscores
+    printf("| /  |   |   |\n");
+    printf("|/   |   |   |\n");
+    printf("|\\   |   |   |\n");
+    printf("| \\  |   |   |\n");
+
+    return 0;
+}
