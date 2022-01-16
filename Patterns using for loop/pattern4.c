3c113f301773ece9cabad634a0ec761eec9bcdb8
Soumyajit Roy <78533174+roysammy123@users.noreply.github.com>
2022-01-16 19:24:45 +0530

    Create pattern4.c

---
+++ Patterns using for loop/pattern4.c
@@ -0,0 +1,23 @@
+//Program to print a number pattern
+
+#include<stdio.h>
+
+int main()
+{
+    int n,i,j;
+    printf("Enter the number of rows\n");
+    scanf("%d",&n);
+    printf("The desired pattern is:\n\n");
+
+    for(i=1;i<=n;i++)
+    {
+        for(j=i;j>=1;j--)
+        {
+            printf("%d",j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
