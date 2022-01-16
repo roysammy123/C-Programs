86893a201ffb33f1ce91598db53ec42ae90c7a65
Soumyajit Roy <78533174+roysammy123@users.noreply.github.com>
2022-01-16 19:29:03 +0530

    Create pattern_cross.c

---
+++ Patterns using for loop/pattern_cross.c
@@ -0,0 +1,31 @@
+//Program to print a pattern 
+
+#include<stdio.h>
+
+int main()
+{
+    int i,j,n;
+    printf("Enter the number of rows (enter a postive odd integer)\n");
+    scanf("%d",&n);
+    printf("The desired output is:\n\n");
+
+    for(i=0;i<n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(j==i+1 || j==n-i)
+            {
+                printf("*");
+            }
+
+            else
+            {
+                printf(" ");
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
