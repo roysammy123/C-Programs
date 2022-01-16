917bf9e4375584aeaa9a4e9e8a056fce46faebe7
Soumyajit Roy <78533174+roysammy123@users.noreply.github.com>
2022-01-16 19:13:54 +0530

    Create factorialsumseries.c

---
+++ while & if-else/factorialsumseries.c
@@ -0,0 +1,27 @@
+//Program to print the sum of 1/1! + 1/3! + 1/5! +.....+ 1/n!
+
+#include<stdio.h>
+
+int main()
+{
+    int n,i,prd,tmp;
+    float sum=0;
+    printf("Enter a positive odd number\n");
+    scanf("%d",&n);
+    i=1;
+    while (i<=n)
+    {
+        tmp=i;
+        prd=1;
+        while (tmp>=1)
+        {
+            prd=prd*tmp;
+            tmp--;
+        }
+
+        sum=sum+(1.0/prd);
+        i=i+1;
+    }
+
+    printf("The result of 1/1! + 1/3! + 1/5! +.....+ 1/n! is %f",sum);
+}
