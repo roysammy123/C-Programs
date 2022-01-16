6ee074c0b16e23f039bdf22d4059462d11fc11aa
Soumyajit Roy <78533174+roysammy123@users.noreply.github.com>
2022-01-16 18:18:49 +0530

    Create address.c

---
+++ Escape Characters/address.c
@@ -0,0 +1,13 @@
+//Program to diplay name and address using escape characters
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Name\t: Soumyajit Roy\n");
+    printf("Address\t: XYZ ABC Street\n");
+    printf("\t  Top Floor, Flat XX\n");
+    printf("\t  Near PQR building\n");
+    printf("City\t: ABCDE\n");
+    printf("PIN\t: XXXXXX\n");
+}
